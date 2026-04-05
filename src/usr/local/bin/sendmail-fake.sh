f6cb017993999216ca8c21534ba761cbc2b62960
Antonio Salsi <passy.linux@zresa.it>
2026-04-05 13:24:10 +0200

    feat: increase maximum body lines in sendmail-fake.sh and adjust notification timeout

--- src/usr/local/bin/sendmail-fake.sh
+++ src/usr/local/bin/sendmail-fake.sh
@@ -3,7 +3,7 @@
 MESSAGE=$(cat)
 
 # Maximum number of body lines to include in the notification
-MAX_BODY_LINES=5
+MAX_BODY_LINES=7
 
 SUBJECT=$(printf '%s' "$MESSAGE" | grep -i '^Subject:' | head -1 | sed 's/^[Ss]ubject:[[:space:]]*//')
 BODY=$(printf '%s' "$MESSAGE" | sed -n '/^$/,$ p' | sed '1d' | head -n "$MAX_BODY_LINES")
@@ -25,10 +25,14 @@ if [ -z "$DBUS_ADDR" ]; then
     DBUS_ADDR="unix:path=/run/user/1000/bus"
 fi
 
+if [ -z "$BODY" ]; then
+    BODY==$(printf '%s' "$MESSAGE" | head -n "$MAX_BODY_LINES")
+fi
+
+
 sudo -u antoniosalsi \
     DISPLAY=:0 \
     DBUS_SESSION_BUS_ADDRESS="$DBUS_ADDR" \
-    notify-send -t 5000 "$SUBJECT" "$BODY" --icon=dialog-information \
-    --icon=dialog-warning -u critical
+    notify-send -t 2500 "$SUBJECT" "$BODY" --icon=dialog-information -u normal
 
 exit 0
